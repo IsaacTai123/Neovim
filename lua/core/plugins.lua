ae572318dc9f6ac55ce343cda9f55ccb7cfe076d
Isaac T <kevincurry438@gmail.com>
2024-07-01 09:47:03 +0800

    Add GitHub Copilot package

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -92,6 +92,9 @@ return require('packer').startup(function(use)
   -- View
   use { "Pocco81/true-zen.nvim" }
 
+  -- Copilot
+  use 'github/copilot.vim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
