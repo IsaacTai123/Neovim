4c43e5aa8c2108bb29de9e81835641441922351e
Isaac T <kevincurry438@gmail.com>
2025-07-01 14:08:37 -0400

    fix: mason-lspconfig handlers not running (new api provided vim.lsp.config)

--- lua/core/lsp/mason-lspconfig.lua
+++ lua/core/lsp/mason-lspconfig.lua
@@ -1,23 +1,23 @@
 local helper = require("core.helper")
 local mason_lspconfig = helper.safe_require("mason-lspconfig")
-local lspconfig = helper.safe_require("lspconfig")
-local capabilities = require('blink.cmp').get_lsp_capabilities()
+local capabilities = helper.safe_require('blink.cmp').get_lsp_capabilities()
 local servers = require("core.lsp.servers")
 
-
-mason_lspconfig.setup {
+mason_lspconfig.setup({
 	ensure_installed = {}, -- explicitly set to an empty table (we already handled this via mason-tool-installer)
-	automatic_enable = false,
-	automatic_installation = false,
-	handlers = {
-		function(server_name)
-			local server = servers[server_name] or {}
+	automatic_enable = true;
 
-			-- This handles overriding only values explicitly passed
-			-- by the server configuration above. Useful when disabling
-			-- certain features of an LSP (for example, turning off formatting for ts_ls)
-			server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-			lspconfig[server_name].setup(server)
-		end,
-	},
-}
+	-- To only enable certain servers to be automatically enabled:
+	-- automatic_enable = vim.tbl_keys(servers),
+})
+
+-- Neovim 0.11 introduces a new API `vim.lsp.config()` for declaratively registering LSP settings.
+-- This replaces the old `lspconfig[server].setup()` and mason-lspconfig's setup_handlers.
+-- Actual LSP activation is triggered by `vim.lsp.enable()` (which automatic_enable handles).
+for server_name, config in pairs(servers) do
+	-- print("Setting up LSP for:", server_name)
+	config = config or {}
+	config.capabilities = vim.tbl_deep_extend('force', {}, capabilities, config.capabilities or {})
+
+	vim.lsp.config(server_name, config)
+end
